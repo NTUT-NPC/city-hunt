b4cf9228c410c67fefb1526b2420dd0e59465c55
darkfanxing <darkfanxing@gmail.com>
2019-04-19 21:42:12 +0800

    Delete the v-app tag, and fix the error

--- src/components/btn-dialog.vue
+++ src/components/btn-dialog.vue
@@ -1,33 +1,31 @@
 <template>
-  <v-app>
-    <v-dialog v-model="dialog" width="500">
-      <template v-slot:activator="{ on }">
-        <v-btn color="red lighten-2" dark v-on="on">詳細資訊</v-btn>
-      </template>
+  <v-dialog v-model="dialog" width="500">
+    <template v-slot:activator="{ on }">
+      <v-btn color="red lighten-2" dark v-on="on">詳細資訊</v-btn>
+    </template>
 
-      <v-card>
-        <v-card-title class="headline lighten-2">
-          <v-layout justify-center>
-            <h2 class="my-3">調查蔡家兄弟</h2>
-          </v-layout>
-        </v-card-title>
+    <v-card>
+      <v-card-title class="headline lighten-2">
+        <v-layout justify-center>
+          <h2 class="my-3">調查蔡家兄弟</h2>
+        </v-layout>
+      </v-card-title>
 
-        <v-card-text>
-          <v-container text-xs-center>
-            <p>蔡氏兄弟時常出現在碼頭，詢問他們是否有更多線索吧！</p>
-            <img src="https://i.imgur.com/aNFzbsL.png">
-          </v-container>
-        </v-card-text>
+      <v-card-text>
+        <v-container text-xs-center>
+          <p>蔡氏兄弟時常出現在碼頭，詢問他們是否有更多線索吧！</p>
+          <img src="https://i.imgur.com/aNFzbsL.png">
+        </v-container>
+      </v-card-text>
 
-        <v-divider></v-divider>
+      <v-divider></v-divider>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" flat @click="dialog = false">額外提示</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-app>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" flat @click="dialog = false">額外提示</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
