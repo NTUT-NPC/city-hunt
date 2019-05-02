9825d3782d372278e7227a7e431c2baa6fd31667
darkfanxing <darkfanxing@gmail.com>
2019-05-02 21:36:24 +0800

    Mission 1 components Initalization

---
+++ src/components/Mission1/Mission1-2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-img :src="require('@/assets/Mission1/1_2.png')" width="100%" max-width="600px"></v-img>
+    <Navbar></Navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ src/components/Mission1/Mission1-4.vue
@@ -0,0 +1,3 @@
+<template>
+  <v-img :src="require('@/assets/Mission1/1_4.png')" width="100%" max-width="600px"></v-img>
+</template>
