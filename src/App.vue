5663071cc2534bcc5a91d34a909632c526615a01
DevilTea <ch870814@gmail.com>
2019-05-03 16:13:07 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="app"></div>
+  <v-app>
+    <v-content>
+      <v-container class="ma-0 pa-0" fluid fill-height></v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <style lang="scss">
