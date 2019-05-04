6be6b964ece886e047f92874cc97376fef40064b
darkfanxing <darkfanxing@gmail.com>
2019-05-04 12:28:39 +0800

    Modify data(bottomNav) to props

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,5 +1,4 @@
 <template>
-    <v-card height="100%">
     <v-bottom-nav
       :active.sync="bottomNav"
       :color="color"
@@ -9,44 +8,42 @@
       shift
     >
       <v-btn dark>
-        <span>任務</span>
-        <v-icon>calendar_today</v-icon>
+        <span>Video</span>
+        <v-icon>ondemand_video</v-icon>
       </v-btn>
 
       <v-btn dark>
-        <span>回顧</span>
-        <v-icon>access_time</v-icon>
+        <span>Music</span>
+        <v-icon>music_note</v-icon>
       </v-btn>
 
       <v-btn dark>
-        <span>背包</span>
-        <v-icon>work_outline</v-icon>
+        <span>Book</span>
+        <v-icon>book</v-icon>
       </v-btn>
 
       <v-btn dark>
-        <span>角色</span>
-        <v-icon>people</v-icon>
+        <span>Image</span>
+        <v-icon>image</v-icon>
       </v-btn>
     </v-bottom-nav>
-  </v-card>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            bottomNav: 3,
-        }
-    },
-    computed: {
-      color () {
-        switch (this.bottomNav) {
-          case 0: return 'blue-grey'
-          case 1: return '#43A047'
-          case 2: return 'brown'
-          case 3: return 'indigo'
-        }
+  props: [
+    'bottomNav'
+  ],
+  computed: {
+    color () {
+      switch (this.bottomNav) {
+        case 0: return 'blue-grey'
+        case 1: return 'teal'
+        case 2: return 'brown'
+        case 3: return 'indigo'
+        default: return 'transparent'
       }
     }
+  }
 }
 </script>
