435cde329cf2fa9b7a7e1fdc4cad540fbae27f9a
darkfanxing <darkfanxing@gmail.com>
2019-05-04 12:22:14 +0800

    Modify the Mission1-1 to Mission1-8, deland delete the Mission 1-9

--- src/components/Mission1/Mission1-2.vue
+++ src/components/Mission1/Mission1-2.vue
@@ -1,16 +1,3 @@
 <template>
-  <div>
-    <v-img :src="require('@/assets/Mission1/1_2.png')" width="100%" max-width="600px"></v-img>
-    <Navbar></Navbar>
-  </div>
+  <v-img :src="require('@/assets/Mission1/1_2.png')" width="100%" max-width="600px"></v-img>
 </template>
-
-<script>
-import Navbar from '@/components/Navbar'
-
-export default {
-  components: {
-    Navbar
-  }
-}
-</script>
---
+++ src/components/Mission1/Mission1-8.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>輸入正確！點擊以下一頁</h2>
+</template>
