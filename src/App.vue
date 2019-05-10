b79225f098446391dcef5701a553a9f4caeeb0c7
yaoandy107 <yaoandy107@gmail.com>
2019-05-10 05:59:56 +0000

    Modify font

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,7 @@ export default {
 
 <style>
 #app {
-  font-family: Helvetica, Arial, "文泉驛正黑", "WenQuanYi Zen Hei", "儷黑 Pro",
-    "LiHei Pro", "黑體-繁", "Heiti TC", "微軟正黑體", "Microsoft JhengHei",
+  font-family: Helvetica, Arial, "文泉驛正黑", "WenQuanYi Zen Hei", "黑體-繁", "Heiti TC", "微軟正黑體", "Microsoft JhengHei",
     sans-serif !important;
 }
 </style>
