1558e7af6476ee10bb0139cacfe97ede76a6bfa5
darkfanxing <darkfanxing@gmail.com>
2019-05-10 19:35:15 +0800

    Modify the js bug

--- src/components/Mission3.vue
+++ src/components/Mission3.vue
@@ -61,11 +61,11 @@ export default {
   },
   methods: {
     nextStory () {
-      if (this.story === 5) {
-        localStorage.story = 1
+      if (+this.story === 5) {
+        localStorage.story = +1
         this.$emit('Mission', '4')
       } else {
-        this.story += 1
+        this.story = +this.story + 1
         localStorage.story = +this.story
       }
     },
