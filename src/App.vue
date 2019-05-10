d2a1ac292248a3cb1796c8dc5f5108c54c197974
darkfanxing <darkfanxing@gmail.com>
2019-05-10 10:58:37 +0800

    Add font

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,11 @@ export default {
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, "文泉驛正黑", "WenQuanYi Zen Hei", "儷黑 Pro",
+    "LiHei Pro", "黑體-繁", "Heiti TC", "微軟正黑體", "Microsoft JhengHei",
+    sans-serif !important;
+}
+</style>
