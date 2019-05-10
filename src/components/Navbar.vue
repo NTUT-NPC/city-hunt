39ddb8b521b6c95440268f8c4d2cfed2abe5caa7
darkfanxing <darkfanxing@gmail.com>
2019-05-10 11:00:58 +0800

    Delete the review and character

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,18 +13,8 @@
       </v-btn>
 
       <v-btn dark>
-        <span>回顧</span>
-        <v-icon>access_time</v-icon>
-      </v-btn>
-
-      <v-btn dark>
         <span>背包</span>
         <v-icon>work_outline</v-icon>
-      </v-btn>
-
-      <v-btn dark>
-        <span>角色</span>
-        <v-icon>people</v-icon>
       </v-btn>
     </v-bottom-nav>
 </template>
@@ -41,8 +31,6 @@ export default {
       switch (this.bottomNav) {
         case 0: return 'blue-grey'
         case 1: return '#43A047'
-        case 2: return 'brown'
-        case 3: return 'indigo'
         default: return 'transparent'
       }
     }
