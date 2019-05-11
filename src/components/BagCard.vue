db19f47b38f8e1a65593c5737fca22565cb8b73f
henry3556108 <36980686+henry3556108@users.noreply.github.com>
2019-05-11 11:12:21 +0800

    Modify which items need to show

--- src/components/BagCard.vue
+++ src/components/BagCard.vue
@@ -95,27 +95,27 @@ export default {
   data () {
     return {
       items: [
-        { name: '線索1-信封', describe: 'this is 匕首 describe', img: require('@/assets/item/ch2線索1_信封.png'), show: false },
-        { name: '線索2-信', describe: 'this is 弓箭 describe', img: require('@/assets/item/ch2線索2_信.png'), show: false },
-        { name: '地址號碼解說', describe: 'this is 盾牌 describe', img: require('@/assets/item/ch2線索3_地址號碼解說.jpg'), show: false },
-        { name: '第一部分線索1', describe: 'this is 匕首 describe', img: require('@/assets/item/ch3第一部分線索1.png'), show: false },
-        { name: '第二部分地圖', describe: 'this is 弓箭 describe', img: require('@/assets/item/ch3第二部分地圖.png'), show: false },
-        // { name: '文達給永嘉的信', describe: 'this is 盾牌 describe', img: require('@/assets/item/ch4 盒內的 文達給永嘉的信.jpg'), show: false },
-        // { name: '盒內的商人ab合照', describe: 'this is 匕首 describe', img: require('@/assets/item/ch4 盒內的商人ab合照.png'), show: false },
-        // { name: '盒內的永嘉自白信', describe: 'this is 弓箭 describe', img: require('@/assets/item/ch4 盒內的永嘉自白信.jpg'), show: false },
-        // { name: '盒子開', describe: 'this is 盾牌 describe', img: require('@/assets/item/ch4盒子開.PNG'), show: false },
-        // { name: '盒子關', describe: 'this is 匕首 describe', img: require('@/assets/item/ch4盒子關.PNG'), show: false },
-        { name: 'ch4第一部份-線索1', describe: 'this is 弓箭 describe', img: require('@/assets/item/ch4第一部份-線索1.jpg'), show: false },
-        { name: 'ch4第一部份_線索2', describe: 'this is 盾牌 describe', img: require('@/assets/item/ch4第一部份_線索2.jpg'), show: false },
-        { name: 'ch4第二部分線索-水門照', describe: 'this is 匕首 describe', img: require('@/assets/item/ch4第二部分線索-水門照.jpg'), show: false },
-        // { name: 'ch5八卦謎題-法器', describe: 'this is 弓箭 describe', img: require('@/assets/item/ch5八卦謎題-法器.png'), show: false },
-        // { name: 'ch5八卦謎題線索-旅人筆記', describe: 'this is 盾牌 describe', img: require('@/assets/item/ch5八卦謎題線索-旅人筆記.png'), show: false },
-        { name: '信壓-01.png', describe: 'this is 匕首 describe', img: require('@/assets/item/信壓-01.png'), show: false },
-        { name: '信封.png', describe: 'this is 弓箭 describe', img: require('@/assets/item/信封.png'), show: false },
-        { name: '實體道具MAP1', describe: 'this is 盾牌 describe', img: require('@/assets/item/實體道具MAP1.png'), show: false },
-        { name: '實體道具七星山由來', describe: 'this is 匕首 describe', img: require('@/assets/item/實體道具七星山由來.png'), show: false },
-        { name: '實體道具勺勺', describe: 'this is 法杖 describe', img: require('@/assets/item/實體道具勺勺.jpg'), show: false },
-        { name: '實體道具吃的跟勺', describe: 'this is 匕首 describe', img: require('@/assets/item/實體道具吃的跟勺.png'), show: false }
+        { name: '信封', describe: 'this is 匕首 的描述', img: require('@/assets/item/ch2線索1_信封.png'), show: false },
+        { name: '信', describe: 'this is 弓箭 的描述', img: require('@/assets/item/ch2線索2_信.png'), show: false },
+        { name: '地址號碼解說', describe: 'this is 盾牌 的描述', img: require('@/assets/item/ch2線索3_地址號碼解說.jpg'), show: false },
+        { name: '線索1', describe: 'this is 匕首 的描述', img: require('@/assets/item/ch3第一部分線索1.png'), show: false },
+        { name: '地圖', describe: 'this is 弓箭 的描述', img: require('@/assets/item/ch3第二部分地圖.png'), show: false },
+        // { name: '文達給永嘉的信', describe: 'this is 盾牌 的描述', img: require('@/assets/item/ch4 盒內的 文達給永嘉的信.jpg'), show: false },
+        // { name: '盒內的商人ab合照', describe: 'this is 匕首 的描述', img: require('@/assets/item/ch4 盒內的商人ab合照.png'), show: false },
+        // { name: '盒內的永嘉自白信', describe: 'this is 弓箭 的描述', img: require('@/assets/item/ch4 盒內的永嘉自白信.jpg'), show: false },
+        // { name: '盒子開', describe: 'this is 盾牌 的描述', img: require('@/assets/item/ch4盒子開.PNG'), show: false },
+        // { name: '盒子關', describe: 'this is 匕首 的描述', img: require('@/assets/item/ch4盒子關.PNG'), show: false },
+        { name: '線索1', describe: 'this is 弓箭 的描述', img: require('@/assets/item/ch4第一部份-線索1.jpg'), show: false },
+        { name: '線索2', describe: 'this is 盾牌 的描述', img: require('@/assets/item/ch4第一部份_線索2.jpg'), show: false },
+        { name: '水門照', describe: 'this is 匕首 的描述', img: require('@/assets/item/ch4第二部分線索-水門照.jpg'), show: false },
+        // { name: 'ch5八卦謎題-法器', describe: 'this is 弓箭 的描述', img: require('@/assets/item/ch5八卦謎題-法器.png'), show: false },
+        { name: 'ch5八卦謎題線索-旅人筆記', describe: '旅人的筆記......', img: require('@/assets/item/ch5八卦謎題線索-旅人筆記.png'), show: false }
+        // { name: '信壓-01.png', describe: 'this is 匕首 的描述', img: require('@/assets/item/信壓-01.png'), show: false },
+        // { name: '信封.png', describe: 'this is 弓箭 的描述', img: require('@/assets/item/信封.png'), show: false },
+        // { name: '實體道具MAP1', describe: 'this is 盾牌 的描述', img: require('@/assets/item/實體道具MAP1.png'), show: false },
+        // { name: '實體道具七星山由來', describe: 'this is 匕首 的描述', img: require('@/assets/item/實體道具七星山由來.png'), show: false },
+        // { name: '實體道具勺勺', describe: 'this is 法杖 的描述', img: require('@/assets/item/實體道具勺勺.jpg'), show: false },
+        // { name: '實體道具吃的跟勺', describe: 'this is 匕首 的描述', img: require('@/assets/item/實體道具吃的跟勺.png'), show: false }
 
       ]
     }
