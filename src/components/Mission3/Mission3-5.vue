a37408fa2da4c5b28e326e03438260ad5b13dc06
DevilTea <ch870814@gmail.com>
2019-05-12 16:00:46 +0800

    Add mission3-5

--- src/components/Mission3/Mission3-5.vue
+++ src/components/Mission3/Mission3-5.vue
@@ -1,3 +1,186 @@
 <template>
-  <h2 class="px-5 text-xs-center">輸入正確！<br/>點擊以進入下一個任務</h2>
+  <v-layout class="py-4" column align-center justify-center fill-height ref="image">
+    <h2 class="display-1 font-weight-bold">找到四家藥舖</h2>
+    <v-dialog
+      :value="locateDialog && !currentTarget.completed"
+      @input="locateDialog = $event"
+      width="300"
+    >
+      <v-card class="pa-3">
+        <v-layout column justify-center align-center>
+          <h3 class="display-1 font-weight-bold" v-text="currentTarget.tag"></h3>
+          <locate
+            :key="`locate-${currentTarget.tag}`"
+            :target="currentTarget.location"
+            @locate="currentTarget.completed = $event"
+          ></locate>
+        </v-layout>
+      </v-card>
+    </v-dialog>
+    <div class="ma-4">
+      <v-layout row wrap>
+        <v-flex
+          v-for="(target, index) in targets"
+          :key="index"
+          xs6
+          class="text-xs-center"
+          style="height: fit-content;"
+        >
+          <v-btn
+            round
+            outline
+            :color="target.completed ? 'green' : 'blue'"
+            class="headline"
+            @click="clickButton(target)"
+          >{{ target.tag }}</v-btn>
+          <v-card></v-card>
+        </v-flex>
+      </v-layout>
+    </div>
+    <div>
+      <v-dialog v-model="hintDialog1" width="300">
+        <template v-slot:activator="{ on }">
+          <v-btn color="red lighten-2" dark v-on="on" large round>初級提示</v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="headline lighten-2" primary-title>初級提示</v-card-title>
+
+          <v-card-text>
+            <h2 class="text-xs-center">地圖由北到南</h2>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" flat @click="hintDialog1 = false">back</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+    <div>
+      <v-dialog v-model="hintDialog2" width="300">
+        <template v-slot:activator="{ on }">
+          <v-btn color="red lighten-2" dark v-on="on" large round>中級提示</v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="headline lighten-2" primary-title>中級提示</v-card-title>
+
+          <v-card-text>
+            <h2 class="text-xs-center">注意羅馬數字後的數字</h2>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" flat @click="hintDialog2 = false">back</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+    <div>
+      <v-dialog v-model="hintDialog3" width="300">
+        <template v-slot:activator="{ on }">
+          <v-btn color="red lighten-2" dark v-on="on" round large>高級提示</v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="headline lighten-2" primary-title>高級提示</v-card-title>
+
+          <v-card-text>
+            <h2 class="text-xs-center">數字就是格子的順序</h2>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" flat @click="hintDialog3 = false">back</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+    <v-dialog :value="completed" max-width="300" persistent>
+      <v-card>
+        <v-card-title primary-title>哇！你真棒</v-card-title>
+        <v-card-text>恭喜已找齊了四家藥舖！</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat @click="NextView">下一步</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
 </template>
+
+<script>
+import Locate from '@/components/Locate'
+export default {
+  created () {
+    this.$emit('inputJudge', true)
+  },
+  computed: {
+    completed () {
+      for (let target of this.targets) {
+        if (!target.completed) {
+          return false
+        }
+      }
+      return true
+    }
+  },
+  components: {
+    Locate
+  },
+  data () {
+    return {
+      currentTarget: {},
+      targets: [
+        {
+          tag: 'ほ',
+          location: {
+            lat: 25.057297,
+            lng: 121.509715
+          },
+          completed: false
+        },
+        {
+          tag: 'む',
+          location: {
+            lat: 25.058729,
+            lng: 121.509634
+          },
+          completed: false
+        },
+        {
+          tag: 'し',
+          location: {
+            lat: 25.054674,
+            lng: 121.510302
+          },
+          completed: false
+        },
+        {
+          tag: 'ふ',
+          location: {
+            lat: 25.055746,
+            lng: 121.509894
+          },
+          completed: false
+        }
+      ],
+      hintDialog1: false,
+      hintDialog2: false,
+      hintDialog3: false,
+      locateDialog: false
+    }
+  },
+  methods: {
+    NextView () {
+      this.$emit('inputJudge', false)
+      this.$refs.image.click()
+    },
+    clickButton (target) {
+      this.currentTarget = target
+      this.locateDialog = true
+    }
+  }
+}
+</script>
