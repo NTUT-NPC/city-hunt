8d7281e8b9f523346578768f85a7b1df4831961a
darkfanxing <darkfanxing@gmail.com>
2019-05-14 20:45:52 +0800

    Add the Mission 5-4 prompt

--- src/components/Mission5/Mission5-4.vue
+++ src/components/Mission5/Mission5-4.vue
@@ -38,6 +38,133 @@
         >
           確認
         </v-btn>
+        <div class="text-xs-center mt-4">
+                <div>
+                    <div class="text-xs-center">
+                        <v-dialog
+                        v-model="dialog1"
+                        width="300"
+                        >
+                        <template v-slot:activator="{ on }">
+                            <v-btn
+                            color="#ffc18c"
+                            v-on="on"
+                            large
+                            round
+                            >
+                            初級提示
+                            </v-btn>
+                        </template>
+
+                        <v-card>
+                            <v-card-title
+                            class="headline lighten-2"
+                            primary-title
+                            >
+                            初級提示
+                            </v-card-title>
+
+                            <v-card-text>
+                              <h2 class="text-xs-center">請仔細觀察地上</h2>
+                            </v-card-text>
+
+                            <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                color="primary"
+                                flat
+                                @click="dialog1 = false"
+                            >
+                                back
+                            </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                        </v-dialog>
+                    </div>
+                </div>
+                <div>
+                    <v-dialog
+                        v-model="dialog2"
+                        width="300"
+                        >
+                        <template v-slot:activator="{ on }">
+                            <v-btn
+                            color="#ffc18c"
+                            v-on="on"
+                            large
+                            round
+                            >
+                            中級提示
+                            </v-btn>
+                        </template>
+
+                        <v-card>
+                            <v-card-title
+                            class="headline lighten-2"
+                            primary-title
+                            >
+                            中級提示
+                            </v-card-title>
+
+                            <v-card-text>
+                              <h2 class="text-xs-center">二進位</h2>
+                            </v-card-text>
+
+                            <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                color="primary"
+                                flat
+                                @click="dialog2 = false"
+                            >
+                                back
+                            </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                        </v-dialog>
+                </div>
+                <div>
+                    <v-dialog
+                        v-model="dialog3"
+                        width="300"
+                        >
+                        <template v-slot:activator="{ on }">
+                            <v-btn
+                            color="#ffc18c"
+                            v-on="on"
+                            round
+                            large
+                            >
+                            高級提示
+                            </v-btn>
+                        </template>
+
+                        <v-card>
+                            <v-card-title
+                            class="headline lighten-2"
+                            primary-title
+                            >
+                            高級提示
+                            </v-card-title>
+
+                            <v-card-text>
+                              <h2 class="text-xs-center">坤：0, 震：1, 坎：2, 兌：3, 艮：4, 離：5, 巽：6, 乾：7</h2>
+                            </v-card-text>
+
+                            <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                color="primary"
+                                flat
+                                @click="dialog3 = false"
+                            >
+                                back
+                            </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                        </v-dialog>
+                </div>
+            </div>
     </v-layout>
 </template>
 
@@ -45,6 +172,9 @@
 export default {
   data () {
     return {
+      dialog1: false,
+      dialog2: false,
+      dialog3: false,
       hasError: false,
       nameRules: [
         v => !!v || '你還沒有輸入此欄喔！'
