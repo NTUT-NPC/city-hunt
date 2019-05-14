8fb548b3615a803e7dfedd3b7dea07dc69fb1907
darkfanxing <darkfanxing@gmail.com>
2019-05-14 19:07:39 +0800

    Update the select image function

--- src/components/Mission4/Mission4-8.vue
+++ src/components/Mission4/Mission4-8.vue
@@ -8,23 +8,35 @@
 export default {
   data () {
     return {
-      imageURL: require('@/assets/Mission4/4-3-12a.jpg'),
-      image: 12,
+      imageURL: require('@/assets/Mission4/4-3-13a.jpg'),
+      image: 13,
       ending: 'a'
     }
   },
   created () {
     this.$emit('inputJudge', true)
-    // 待做判斷
+  },
+  mounted () {
+    this.ending = localStorage.ending || 'a'
+    this.imageURL = require(`@/assets/Mission4/4-3-${this.image + this.ending}.jpg`)
   },
   methods: {
     NextImage () {
       this.image += 1
-      if (this.image >= 14) {
-        this.$emit('inputJudge', false)
-        this.$refs.image.click()
+      if (this.ending === 'a') {
+        if (this.image >= 15) {
+          this.$emit('inputJudge', false)
+          this.$refs.image.click()
+        } else {
+          this.imageURL = require(`@/assets/Mission4/4-3-${this.image + this.ending}.jpg`)
+        }
       } else {
-        this.imageURL = require(`@/assets/Mission4/4-3-${this.image + this.ending}.jpg`)
+        if (this.image >= 16) {
+          this.$emit('inputJudge', false)
+          this.$refs.image.click()
+        } else {
+          this.imageURL = require(`@/assets/Mission4/4-3-${this.image + this.ending}.jpg`)
+        }
       }
     }
   }
