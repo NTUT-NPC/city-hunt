148d480121dc0ce39f45e2ee7cfa87c02495f8c3
darkfanxing <darkfanxing@gmail.com>
2019-05-16 20:51:51 +0800

    Update the ending

--- src/components/Mission6/Mission6-3.vue
+++ src/components/Mission6/Mission6-3.vue
@@ -1,6 +1,6 @@
 <template>
     <a href="https://forms.gle/HmYmpASr2t3rnSvr9">
-      <img src="@/assets/Mission6/6-2.png" style="width: 100%;max-width: 800px;">
+      <img src="@/assets/Mission6/ending.png" style="width: 100%;max-width: 800px;">
     </a>
 </template>
 
